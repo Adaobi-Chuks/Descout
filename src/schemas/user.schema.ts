4e8dafd4df4cb6b461bd3b74d25f83b8c4e3b848
fathom329 <chuksaginamada@gmail.com>
2024-04-27 12:06:01 +0100

    Feat: Update userSchema file

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -2,12 +2,12 @@ import Joi from "joi";
 
 const signupSchema = Joi.object({
     email: Joi.string().email().required().lowercase().trim(),
-    username: Joi.string().required().min(8).trim(),
+    companyName: Joi.string().required().trim(),
     password: Joi.string().required().min(8)
 });
 
 const loginSchema = Joi.object({
-    username: Joi.string().required().trim(),
+    email: Joi.string().required().email().trim(),
     password: Joi.string().required().min(8)
 });
 
