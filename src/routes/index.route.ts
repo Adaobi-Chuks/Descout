b4d7c8ab2c7e6c48d31489e345727b02bd6ffa14
fathom329 <chuksaginamada@gmail.com>
2024-04-30 12:08:29 +0100

    Feat: Update index route to include new methods

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -2,7 +2,6 @@ import { Router, Request, Response, Application } from 'express';
 import userRoute from "./user.route";
 import authRoute from "./auth.route";
 import driverRoute from "./driver.route";
-import { BASEPATH } from '../configs/constants.config';
 import { OK } from '../utils/statusCodes.util';
 const router: Router = Router();
 import CustomResponse from "../utils/helpers/response.util";
@@ -12,8 +11,8 @@ router.get("/", (req: Request, res: Response) => {
     return new CustomResponse(OK, true, "Welcome to Descout API ensure to go through the API docs before using this service", res);
 });
 
-// router.use("/users", userRoute);
-// router.use("/auth", authRoute);
+router.use("/user", userRoute);
+router.use("/auth", authRoute);
 router.use("/driver", driverRoute);
 
 export default router;
